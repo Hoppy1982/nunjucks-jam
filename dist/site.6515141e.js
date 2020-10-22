a86ebbc0507636d694408399862fe3f5c035fcd8
Hoppy <mark82hopcraft@gmail.com>
2020-10-22 08:26:23 +0100

    .

--- dist/site.6515141e.js
+++ dist/site.6515141e.js
@@ -160,69 +160,138 @@ navCats.ontransitionend = function (evt) {
 },{}],"../src/js/mh-home-svg.js":[function(require,module,exports) {
 var mhHome = document.getElementById('mh-home');
 var smallML4 = document.querySelector('#mh-home #small-m #line4');
-var animations = [{
+/**
+ * progress often written as 'k' in timing function tutorials.
+ * range is calculated once.
+ * TODO - Possibly, include start & end time for each animation?
+ */
+
+var animatingCoords = [{
   el: smallML4,
   attr: 'x1',
   val0: 25,
   val1: 5,
-  duration: 3000,
-  tFn: tFnLinear
+  range: null,
+  progress: 0
 }, {
   el: smallML4,
   attr: 'y1',
   val0: 123,
   val1: 88,
-  duration: 3000,
-  tFn: tFnLinear
+  range: null,
+  progress: 0
 }, {
   el: smallML4,
   attr: 'x2',
   val0: 25,
   val1: 90,
-  duration: 3000,
-  tFn: tFnLinear
+  range: null,
+  progress: 0
 }, {
   el: smallML4,
   attr: 'y2',
   val0: 25,
   val1: 5,
-  duration: 3000,
-  tFn: tFnLinear
+  range: null,
+  progress: 0
 }];
+animatingCoords.forEach(function (coord) {
+  coord.range = coord.val1 - coord.val0;
+});
+/**
+ * temp
+ */
+
+var currFrame = 0;
+var requestID = null;
+var direction = null;
+/**
+ * step
+ */
+
+function step() {
+  if (start === undefined) {
+    start = timestamp;
+  }
+
+  var elapsed = timestamp - start;
+  currFrame += 1;
+  currFrame += direction;
+  var progress = currFrame / nFrames; // do the things
+
+  animatingCoords[0];
+
+  if (currFrame % nFrames === 0) {
+    stopAnimation();
+    return;
+  }
+
+  requestID = requestAnimationFrame(step);
+}
+/**
+ * stopAnimation
+ */
+
+
+function stopAnimation() {
+  cancelAnimationFrame(requestID);
+  requestID = null;
+}
+
 mhHome.addEventListener('mouseenter', function (evt) {
-  animations.forEach(function (animation) {
+  if (requestID) stopAnimation();
+  direction = 1;
+  updateCoords(animatingCoords);
+}, false);
+/*
+const animations = [
+  { el: smallML4, attr: 'x1', val0: 25, val1: 5, duration: 6000, tFn: tFnEaseIn },
+  { el: smallML4, attr: 'y1', val0: 123, val1: 88, duration: 6000, tFn: tFnEaseIn },
+  { el: smallML4, attr: 'x2', val0: 25, val1: 90, duration: 6000, tFn: tFnEaseIn },
+  { el: smallML4, attr: 'y2', val0: 25, val1: 5, duration: 6000, tFn: tFnEaseIn },
+]
+
+mhHome.addEventListener('mouseenter', (evt) => {
+  animations.forEach(animation => {
     animateAttr(animation.el, animation.attr, animation.val0, animation.val1, animation.duration, animation.tFn);
   });
 });
-mhHome.addEventListener('mouseleave', function (evt) {//const ani = animations[0];
+
+
+mhHome.addEventListener('mouseleave', (evt) => {
+  //const ani = animations[0];
   //animateAttr(ani.el, ani.attr, ani.val1, ani.val0, ani.duration, ani.tFn);
 });
 
-function animateAttr(el, attr, val0, val1, duration, tFn) {
-  var nSteps = duration / 16.67;
-  var isStartValLower = val0 < val1 ? true : false;
-  var spread = isStartValLower ? val1 - val0 : val0 - val1;
-  var start;
 
+function animateAttr(el, attr, val0, val1, duration, tFn) {
+  const deltaPct = 16.67 / duration
+  const isStartValLower = val0 < val1 ? true : false;
+  const spread = isStartValLower ? val1 - val0 : val0 - val1;
+  
   function update(timestamp) {
+    let start;
     if (start === undefined) {
       start = timestamp;
-    } // do the change
-
-
-    var elapsed = timestamp - start;
-    var currVal = el.getAttribute(attr);
-    var currValNum = parseFloat(currVal);
-    var fractionComplete = Math.abs((currValNum - val0) / spread);
-    var newValNum;
+    }
+    const elapsed = timestamp - start;
+    let currVal = el.getAttribute(attr);
+    let currValNum = parseFloat(currVal);
+    let fractionComplete = Math.abs( (currValNum - val0) / spread );
+    fractionComplete += deltaPct;
+    if (fractionComplete > 1) {
+      fractionComplete = 1;
+    }
+    let newValNum;
+    console.log(fractionComplete)
 
     if (isStartValLower) {
-      newValNum = currValNum + 0.8;
+      newValNum = val0 + ( spread * tFn(fractionComplete) );
     } else {
-      newValNum = currValNum - 0.8;
+      newValNum = val0 - ( spread * tFn(fractionComplete) );
     }
 
-    var newVal = newValNum.toString();
+    let newVal = newValNum.toString();
     el.setAttribute(attr, newVal);
 
     if (fractionComplete < 1) {
@@ -232,9 +301,23 @@ function animateAttr(el, attr, val0, val1, duration, tFn) {
 
   window.requestAnimationFrame(update);
 }
+*/
+// Timing functions
 
 function tFnLinear(timeFraction) {
   return timeFraction;
+}
+
+function tFnEaseIn(timeFraction) {
+  return Math.pow(timeFraction, 1.675);
+}
+
+function tFnEaseOut(timeFraction) {
+  return 1 - Math.pow(1 - timeFraction, 1.675);
+}
+
+function tFnEaseInOut(timeFraction) {
+  return .5 * (Math.sin((timeFraction - .5) * Math.PI) + 1);
 }
 },{}],"../src/js/site.js":[function(require,module,exports) {
 "use strict";
@@ -272,7 +355,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58926" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56364" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/js/mh-home-svg.js
+++ src/js/mh-home-svg.js
@@ -1,12 +1,80 @@
 const mhHome = document.getElementById('mh-home');
 const smallML4 = document.querySelector('#mh-home #small-m #line4');
-const animations = [
-  { el: smallML4, attr: 'x1', val0: 25, val1: 5, duration: 3000, tFn: tFnLinear },
-  { el: smallML4, attr: 'y1', val0: 123, val1: 88, duration: 3000, tFn: tFnLinear },
-  { el: smallML4, attr: 'x2', val0: 25, val1: 90, duration: 3000, tFn: tFnLinear },
-  { el: smallML4, attr: 'y2', val0: 25, val1: 5, duration: 3000, tFn: tFnLinear },
-]
 
+/**
+ * progress often written as 'k' in timing function tutorials.
+ * range is calculated once.
+ * TODO - Possibly, include start & end time for each animation?
+ */
+const animatingCoords = [
+  { el: smallML4, attr: 'x1', val0: 25,   val1: 5,  range: null, progress: 0 },
+  { el: smallML4, attr: 'y1', val0: 123,  val1: 88, range: null, progress: 0 },
+  { el: smallML4, attr: 'x2', val0: 25,   val1: 90, range: null, progress: 0 },
+  { el: smallML4, attr: 'y2', val0: 25,   val1: 5,  range: null, progress: 0 },
+]
+animatingCoords.forEach(coord => {
+  coord.range = coord.val1 - coord.val0;
+});
+
+
+/**
+ * temp
+ */
+let currFrame = 0;
+let requestID = null;
+let direction = null;
+
+
+/**
+ * step
+ */
+function step() {
+  if (start === undefined) {
+    start = timestamp;
+  }
+
+  let elapsed = timestamp - start;
+  currFrame +=1;
+
+  currFrame += direction;
+  let progress = currFrame / nFrames; 
+
+  // do the things
+  animatingCoords[0];
+  
+
+  if (currFrame % nFrames === 0) {
+    stopAnimation();
+    return;
+  }
+
+  requestID = requestAnimationFrame(step);
+}
+
+
+/**
+ * stopAnimation
+ */
+function stopAnimation() {
+  cancelAnimationFrame(requestID);
+  requestID = null;  
+}
+
+
+mhHome.addEventListener('mouseenter', (evt) => {
+  if(requestID) stopAnimation();
+  direction = 1;
+  updateCoords(animatingCoords);
+}, false);
+
+
+/*
+const animations = [
+  { el: smallML4, attr: 'x1', val0: 25, val1: 5, duration: 6000, tFn: tFnEaseIn },
+  { el: smallML4, attr: 'y1', val0: 123, val1: 88, duration: 6000, tFn: tFnEaseIn },
+  { el: smallML4, attr: 'x2', val0: 25, val1: 90, duration: 6000, tFn: tFnEaseIn },
+  { el: smallML4, attr: 'y2', val0: 25, val1: 5, duration: 6000, tFn: tFnEaseIn },
+]
 
 mhHome.addEventListener('mouseenter', (evt) => {
   animations.forEach(animation => {
@@ -22,27 +90,30 @@ mhHome.addEventListener('mouseleave', (evt) => {
 
 
 function animateAttr(el, attr, val0, val1, duration, tFn) {
-  const nSteps = duration / 16.67;
+  const deltaPct = 16.67 / duration
   const isStartValLower = val0 < val1 ? true : false;
   const spread = isStartValLower ? val1 - val0 : val0 - val1;
-  let start;
   
   function update(timestamp) {
+    let start;
     if (start === undefined) {
       start = timestamp;
     }
-    
-    // do the change
     const elapsed = timestamp - start;
     let currVal = el.getAttribute(attr);
     let currValNum = parseFloat(currVal);
     let fractionComplete = Math.abs( (currValNum - val0) / spread );
+    fractionComplete += deltaPct;
+    if (fractionComplete > 1) {
+      fractionComplete = 1;
+    }
     let newValNum;
+    console.log(fractionComplete)
 
     if (isStartValLower) {
-      newValNum = currValNum + 0.8;
+      newValNum = val0 + ( spread * tFn(fractionComplete) );
     } else {
-      newValNum = currValNum - 0.8;
+      newValNum = val0 - ( spread * tFn(fractionComplete) );
     }
 
     let newVal = newValNum.toString();
@@ -55,8 +126,27 @@ function animateAttr(el, attr, val0, val1, duration, tFn) {
 
   window.requestAnimationFrame(update);
 }
+*/
 
-
+// Timing functions
 function tFnLinear(timeFraction) {
   return timeFraction;
 }
+
+function tFnEaseIn(timeFraction) {
+  return Math.pow(timeFraction, 1.675);
+}
+
+function tFnEaseOut(timeFraction) {
+  return 1 - Math.pow(1 - timeFraction, 1.675);
+}
+
+function tFnEaseInOut(timeFraction) {
+  return .5*(Math.sin((timeFraction - .5)*Math.PI) + 1);
+}
+
+
+
+
+
+
